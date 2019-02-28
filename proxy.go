3fa4bc262b1765676d1355cac4791155f3e61c81
HIGHER98 <hyerg@tcd.ie>
2019-02-28 15:27:33 +0000

    Attempt so far for caching responses
    Not fully functional at the moment

--- proxy.go
+++ proxy.go
@@ -27,7 +27,17 @@ var (
 		Timeout: 0,
 	}
 	blockedStr string
+	connection = cachedReqResp{"", nil, nil,}
+	cached map[string]cachedReqResp 	//Hashmap of URL to string of filename
 )
+
+type cachedReqResp struct{
+	Host string
+	Request *http.Request
+	Response *http.Response
+}
+
+//To cache websites, have a hashmap[Url:File] that store the html and response headers for each http url
 
 func init(){
 	//Reads in the blocked websites
@@ -38,29 +48,6 @@ func init(){
 	}
 	blockedStr = string(blocked)
 	fmt.Println("Blocked websites:\n", blockedStr)		//Prints content as string
-}
-
-func listenAndForward(r *http.Request) (resp *http.Response, bodyString string){
-	//If host is on blocked list, don't query website, instead return blocked message
-	if isBlocked(r.URL.Host){
-		return nil, "<!DOCTYPE html><html><body><h1>This website has been blocked</h1></body></html>"
-	}
-
-  //Send the request to the destination
-	resp, err := client.Do(r)
-	if err != nil{
-		fmt.Println("\nError getting a response\n")
-		log.Fatal(err)
-	}
-	
-	defer resp.Body.Close()
-	bodybytes, er := ioutil.ReadAll(resp.Body)
-	if er != nil{
-		log.Fatal(er)
-	}
-	//bodyString contains the response body
-	bodyString = string(bodybytes)	
-	return resp, bodyString
 }
 
 //Checks if the host is blocked
@@ -75,15 +62,49 @@ func isBlocked(u string) bool{
 	return false
 }
 
+func listenAndForward(r *http.Request) (resp *http.Response, bodyString string){
+	//If host is on blocked list, don't query website
+	if isBlocked(r.URL.Host){
+		return nil, "<!doctype html><html><body><h1>This website has been blocked</h1></body></html>"
+	}
+
+	//Can store info in request for data to cache results
+	cached = make(map[string]cachedReqResp)
+	//if there is something at cached[r.Host]
+	if _, ok := cached[r.Host]; ok{
+		fmt.Println("I see you've been here before")
+		fmt.Println(cached[r.Host])
+		return nil, ""
+	}else{
+		fmt.Println("This is the first time you've accessed this website")
+		//Send the http request to the destination
+		resp, err := client.Do(r)
+		if err != nil{
+			fmt.Println("\nError getting a response\n")
+			log.Fatal(err)
+		}
+		cached[r.Host] = cachedReqResp{Host: r.Host, Request: r, Response: resp}
+	
+		defer resp.Body.Close()
+		bodybytes, er := ioutil.ReadAll(resp.Body)
+		if er != nil{
+			log.Fatal(er)
+		}
+		//bodyString contains the response body
+		bodyString := string(bodybytes)	
+		return resp, bodyString
+	}
+}
+
 //Generates a http request from a byte slice of data heard on DEFAULT_PORT
 func makeHeader(byteHeader []byte) *http.Request{
-	//The below line reads the request but fills in fields for server to parse
+	//Reads the request but fills in fields for server to parse
 	//As a result of this, if req is sent as a request to the host it does not return the correct response
 	req, err := http.ReadRequest(bufio.NewReader(io.MultiReader(bytes.NewReader(byteHeader))))
 	if err != nil{
 		log.Fatal(err)
 	}
-	//Send request
+		//Send request
 	request, err := http.NewRequest(req.Method, req.URL.String(), req.Body)
 	if err!= nil{
 		log.Fatal(err)
@@ -91,7 +112,7 @@ func makeHeader(byteHeader []byte) *http.Request{
 	return request
 }
 
-func handler(conn net.Conn){
+func handleme(conn net.Conn){
 	buff := make([]byte, 1024)
 	msgLen, err := conn.Read(buff)
 	if err != nil{
@@ -100,15 +121,23 @@ func handler(conn net.Conn){
 	fmt.Println("\n\nRequest from client:\n",string(buff[:msgLen]))
 	req := makeHeader(buff)
 	resp, respBody := listenAndForward(req)
+	
+	if resp == nil{
+	_, err = conn.Write([]byte(respBody))
+		if err != nil{
+			log.Fatal(err)
+		}
+	}
+	fmt.Println("Response header received: ", resp)
 
+	//Write back on the connection
 	_, err = conn.Write([]byte(respBody))
 	if err != nil{
+		fmt.Println("Error writing data back to browser")
 		log.Fatal(err)
-  }
-	fmt.Println("Response header received: ", resp)
+	}
 	defer conn.Close()
 }
-
 
 func main() {
 	ln, err := net.Listen("tcp", DEFAULT_PORT)
@@ -121,6 +150,6 @@ func main() {
 		if err != nil{
 			log.Fatal(err)
 		}
-		go handler(conn)
+		go handleme(conn)
 	}
 }
